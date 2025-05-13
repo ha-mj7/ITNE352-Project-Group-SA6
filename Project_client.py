3f0cf0ddd78389786b64ebb05f0b477f3e3ebda3
ha-mj7 <ah1mjm@gmail.com>
2025-05-13 19:56:21 +0300

    Adding comments

--- Project_client.py
+++ Project_client.py
@@ -1,6 +1,6 @@
 import socket
 
-
+#function to receive all data from the socket assuring that the data is not cut off in the process
 def recv_all(sock, buffer=4096):
     data = b''
     while True:
@@ -9,35 +9,35 @@ def recv_all(sock, buffer=4096):
         if len(chunck) < buffer:
             break
     return data.decode('ascii')
-
+#creating the client socket
 address = ("127.0.0.1", 1025)
 Cname = input('Enter your name: ')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cs:
     cs.connect(address)
     cs.sendall(Cname.encode('ascii'))  
-
+    #showing the user the available options
     while True:
         print("\nOptions:")
         print("a. Show all arrived flights")
         print("b. Show all delayed flights")
         print("c. Search for a particular flight")
         print("d. Quit connection")
-
+        #Asking the user for to enter the option
         choice = input("Choose one of the options above: ").strip()
         if not choice:
             continue
-
+        #sending the inut to the server
         cs.send(choice.encode('ascii'))
-
+        #if the user wants to quit the connection to the server
         if choice in ['d', '4', 'quit']:
             print("============Disconnecting from server============")
             break
-
+        #asking the user for the flight IATA code when entering option c
         if choice in ['c','3']:
             reply = recv_all(cs)
             print(reply)
             fli_iata = input().upper().strip()
             cs.send(fli_iata.encode('ascii'))
-
+        #receiving the server's response
         response = recv_all(cs) 
         print(response)
